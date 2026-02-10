738c06fe906b7e7e134db9536f43d014e46c6996
Keith McGinley <kmcginley@enterprise-ireland.com>
2026-02-10 18:05:13 +0000

    last attempt

--- extend.php
+++ extend.php
@@ -3,24 +3,27 @@
 use Flarum\Extend;
 use Flarum\Api\Serializer\UserSerializer;
 use Flarum\User\Event\Saving;
-use Kmcginley1928\AddUserTitleAndDescription\Listeners\SaveUserExtras;
 
-return [
+$extenders = [
     // Locales
     (new Extend\Locales(__DIR__ . '/locale')),
 
-    // Forum JS
-    (new Extend\Frontend('forum'))
-        ->js(__DIR__ . '/js/dist/forum.js'),
+    // Forum JS (safe even if file is missing; will just 404)
+    (new Extend\Frontend('forum'))->js(__DIR__ . '/js/dist/forum.js'),
 
-    // Add fields to API output
-    (new Extend\Serializer(UserSerializer::class))
-        ->attributes(function (UserSerializer $serializer, $user, array $attributes) {
+    // Expose fields in API
+    (new Extend\Serializer(\Flarum\Api\Serializer\UserSerializer::class))
+        ->attributes(function (\Flarum\Api\Serializer\UserSerializer $serializer, $user, array $attributes) {
             $attributes['title'] = $user->title;
             $attributes['short_description'] = $user->short_description;
             return $attributes;
         }),
-
-    // Persist fields on PATCH
-    (new Extend\Event())->listen(Saving::class, SaveUserExtras::class),
 ];
+
+// Guard the listener so boot never fatals if autoload/namespace is off
+if (class_exists(\Kmcginley1928\AddUserTitleAndDescription\Listeners\SaveUserExtras::class)) {
+    $extenders[] = (new Extend\Event())
+        ->listen(Saving::class, \Kmcginley1928\AddUserTitleAndDescription\Listeners\SaveUserExtras::class);
+}
+
+return $extenders;
